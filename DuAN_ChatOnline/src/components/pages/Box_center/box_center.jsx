d009efa3934ef6952cdf9dd9e6604e3ead60b271
minh-dev204 <buivanminh281124@gmail.com>
2024-09-07 16:27:01 +0700

    comment group

--- DuAN_ChatOnline/src/components/pages/Box_center/box_center.jsx
+++ DuAN_ChatOnline/src/components/pages/Box_center/box_center.jsx
@@ -1,12 +1,13 @@
 import List_chat from "./list_chat"
 import Search_chat from "./search_chat"
-import Main_groupChats from "./create_groupChats/main_groupChats"
+// import Main_groupChats from "./create_groupChats/main_groupChats"
+// comment cái Main groupChats lại
 function Box_center() {
   return <>
     <Search_chat />
     <List_chat />
 
-    <Main_groupChats />
+    {/* <Main_groupChats /> */}
   </>
 }
 
